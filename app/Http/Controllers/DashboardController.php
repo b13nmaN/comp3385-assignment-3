36e1ae2cb40a3d86e06fc3fb84cd0c1e96ef6996
b13nmaN <blenmanjoel@gmail.com>
2024-03-04 11:21:08 -0400

    client information now displays on the dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,16 +11,8 @@ class DashboardController extends Controller
     public function index(): View
     {
         // get all clients from the database 
-        $logoRecords = Client::all(); 
-        $fileUrls = [];
-
-        foreach ($logoRecords as $record) {
-            $filename = $record->company_logo; 
-            // dd($filename);
-            $fileUrls[] = asset('storage/photos/' . $filename);
-        }        
-
-        return view('dashboard', ['fileUrls' => $fileUrls]);
+        $clients = Client::all(); 
+        return view('dashboard', ['clients' => $clients]);
 
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,15 +6,28 @@
     <div class="alert alert-success" role="alert">
         {{ session('onSuccess') }}
     </div>
-@endif
+    @endif
 </div>
 <div class="d-flex justify-content-between">
     <h1 class="display-5 fw-bold text-body-emphasis">Dashboard</h1>
-    <a href="{{ url('/clients/add') }}" class="btn btn-primary"> + Create Clients </a>
+    <div class="d-flex justify-content-center align-items-center">
+        <a href="{{ url('/clients/add') }}" class="btn btn-primary align-middle text-center"> + Create Clients </a>
+    </div>
 </div>
 <p class="lead">Welcome to your dashboard. Here you can manage your account, your clients and much more.</p>
-@foreach ($fileUrls as $fileUrl)
-    <img src="{{ $fileUrl }}" alt="Company Logo">
-@endforeach
-
+<div class="row g-3">
+    @foreach ($clients as $client)
+    <div class="col-md-3"> <!-- Adjust the column size based on your preference -->
+        <div class="card">
+            <div class="card-body text-center">
+                <img src="{{ asset('storage/photos/' . $client->company_logo) }}" alt="Company Logo" class="mb-3">
+                <h5 class="card-title">{{ $client->name }}</h5>
+                <p class="card-text">{{ $client->telephone }}</p>
+                <p class="card-text">{{ $client->email }}</p>
+                <p class="card-text">{{ $client->address }}</p>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
 @endsection
